6940bd0fd84a4d4cf8121163d0541e92a3e7adf0
JKimani77 <jkkimani77@gmail.com>
2021-04-12 11:45:48 +0300

    created classmethod decorators for displaying and finding credentials functions

--- credentials.py
+++ credentials.py
@@ -14,3 +14,20 @@ class Creds:
 
     def rm_creds(self):
         Creds.creds_list.remove(self)
+
+    @classmethod
+    def find_by_acc_name(cls,account):
+        for credential in cls.creds_list:
+            if credential.account == account:
+                return credential
+
+    @classmethod
+    def credential_exists(cls,account):
+        for credential in cls.creds_list:
+            if credential.account == account:
+                return True
+        return False
+
+    @classmethod
+    def reveal_credentials(cls):
+        return cls.creds_list
