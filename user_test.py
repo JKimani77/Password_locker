0a5b039a9d10847bd507e226d5730824f7f464a2
JKimani77 <jkkimani77@gmail.com>
2021-04-12 11:41:08 +0300

    Created a testcase for saving user objects

--- user_test.py
+++ user_test.py
@@ -10,3 +10,10 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_person.nameone,"Yone")
         self.assertEqual(self.new_person.nametwo,"Yasuo")
         self.assertEqual(self.new_person.password,"1234qwer")
+
+    def test_save_user(self):
+        self.new_person.save_person()
+        self.assertEqual(len(User.persons_list),1)
+
+if __name__ == '__main__':
+    unittest.main()
