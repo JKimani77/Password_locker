a9feeac01347c7cd1454490fb226c50074a3221c
JKimani77 <jkkimani77@gmail.com>
2021-04-12 11:47:23 +0300

    created test cases for finding credentials and ran tests

--- credentials_test.py
+++ credentials_test.py
@@ -30,3 +30,24 @@ class TestCreds(unittest.TestCase):
         test_credentials.save_creds()
         self.new_credential.rm_creds()
         self.assertEqual(len(Creds.creds_list),1)
+
+    def test_find_credential_by_name(self):
+        self.new_credential.save_creds()
+        test_credentials = Creds('Pinterest', '1234asdf')
+        test_credentials.save_creds()
+        found_cred = Creds.find_by_acc_name('Pinterest')
+        self.assertEqual(found_cred.account,test_credentials.account)
+
+    def test_credential_exists(self):
+        self.new_credential.save_creds()
+        test_credentials = Creds('Pinterest', '1234asdf')
+        test_credentials.save_creds()
+        credential_exists = Creds.credential_exists('Pinterest')
+        self.assertTrue(credential_exists)
+
+    def test_show_allcreds(self):
+        self.assertEqual(Creds.reveal_credentials(),Creds.creds_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
